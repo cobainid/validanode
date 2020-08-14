58cfa20beaf72df7ccedd41064081bd5aa231a0b
defrindr <defrindr@gmail.com>
2020-08-14 19:50:52 +0700

    fix typo variable name

--- TYPE_VALIDATOR.js
+++ TYPE_VALIDATOR.js
@@ -8,26 +8,24 @@ const setMessage = (rules) => {
     rules = deleteElement(rules, "value");
     rules.property = deleteElement(rules.property, "message");
 
-    list = {};
+    list_data = {};
 
     rules_as_array = toArray(rules);
 
-    rules_as_array.forEach((rule) => {
+    rules_as_array.forEach( rule => {
         if (rule[0] == 'property') {
             rule_as_array = toArray(rule[1]);
 
             rule_as_array.forEach((data) => {
-                list['property.' + data[0]] = data[1];
+                list_data['property.' + data[0]] = data[1];
             });
         } else {
-            list[element[0]] = element[1];
+            list_data[rule[0]] = rule[1];
         }
     });
 
-    arrayList = toArray(list);
-
-    arrayList.forEach((element) => {
-        message = message.replace(`{${element[0]}}`, element[1]);
+    toArray(list_data).forEach( data => {
+        message = message.replace(`{${data[0]}}`, data[1]);
     });
 
     return message;
